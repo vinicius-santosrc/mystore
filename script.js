916ce2b11f8da82c2639942d628a42cf284e0f3f
Vinícius Santos <viniciusssantos.pa@gmail.com>
2023-07-16 23:50:05 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
 let data = new Date()
-let dia = data.getDay()
+let dia = data.getDate()
 let mes = data.getMonth()
 let ano = data.getFullYear()
 
-document.querySelector(`.current-date`).innerHTML = `${dia}/${mes}/${ano}`
+mes = mes + 1
+
+if(dia < 10) {
+    dia = `0` + dia
+}
+
+let mesnome = ``
+
+if(mes == 1) {
+    mesnome = `janeiro`
+}
+
+if(mes == 2) {
+    mesnome = `fevereiro`
+}
+
+if(mes == 3) {
+    mesnome = `março`
+}
+
+if(mes == 4) {
+    mesnome = `abril`
+}
+if(mes == 5) {
+    mesnome = `maio`
+}
+
+if(mes == 6) {
+    mesnome = `junho`
+}
+
+if(mes == 7) {
+    mesnome = `julho`
+}
+
+if(mes == 8) {
+    mesnome = `agosto`
+}
+
+if(mes == 9) {
+    mesnome = `setembro`
+}
+
+if(mes == 10) {
+    mesnome = `outubro`
+}
+
+if(mes == 11) {
+    mesnome = `novembro`
+}
+
+if(mes == 12) {
+    mesnome = `dezembro`
+}
+
+
+if(mes < 10) {
+    mes = `0` + mes
+}
+
+document.querySelector(`.current-date`).innerHTML = `${dia} de ${mesnome} de ${ano}`
